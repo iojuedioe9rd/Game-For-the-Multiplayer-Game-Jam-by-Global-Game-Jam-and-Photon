07085985529c16dd21d62cfdd89608569bca57eb
iojuedioe9rd <catsstudio81@gmail.com>
2022-09-30 18:12:56 +0100

    Add project files.

---
+++ Assets/Scripts/GamePlay.Matchmaking/LobbyManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using TMPro;
+
+namespace GamePlay.Matchmaking
+{
+    public class LobbyManager : MonoBehaviour
+    {
+
+
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
---
+++ Assets/Scripts/GamePlay.PUN/ConnectToServer.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+namespace GamePlay.PUN
+{
+    public class ConnectToServer : MonoBehaviourPunCallbacks
+    {
+
+        public TMP_InputField usernameInput;
+        public TMP_Text buttonText;
+
+        public void OnClickConnect()
+        {
+            if(!(string.IsNullOrEmpty(usernameInput.text)) && usernameInput.text.Length >= 1)
+            {
+                PhotonNetwork.NickName = usernameInput.text;
+                buttonText.text = "Connecting...";
+                PhotonNetwork.ConnectUsingSettings();
+
+            }
+        }
+
+        public override void OnConnectedToMaster()
+        {
+            SceneManager.LoadScene("Lobby");
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
